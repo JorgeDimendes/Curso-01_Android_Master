6bd3dc2d26037a3cdef56b2080a4780727de04de
Jorge <jorgedimendes@hotmail.com>
2023-08-31 08:31:31 -0300

    Iniciando App Loja Virtual

--- 09_AppTemas/app/src/main/java/com/jordev/temas/SegundaTela.kt
+++ 09_AppTemas/app/src/main/java/com/jordev/temas/SegundaTela.kt
@@ -1,11 +1,21 @@
 package com.jordev.temas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jordev.temas.databinding.ActivitySegundaTelaBinding
 
 class SegundaTela : AppCompatActivity() {
+
+    lateinit var binding: ActivitySegundaTelaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_segunda_tela)
+        binding = ActivitySegundaTelaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btPrimeira.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
