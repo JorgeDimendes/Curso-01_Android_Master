fadefae649be61206750dcdcf9a7ad888daee244
Jorge Menezes <jorgedimendes@hotmail.com>
2024-03-16 14:14:01 -0300

    Testo

--- 01_AppGeradorDeSenhas/app/src/main/java/com/example/a01teste/MainActivity.kt
+++ 01_AppGeradorDeSenhas/app/src/main/java/com/example/a01teste/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         binding.btnGerarSenha.setOnClickListener {
             gerarSenhaAleatoria()
         }
+        // teste
     }
 
     private fun gerarSenhaAleatoria(){
